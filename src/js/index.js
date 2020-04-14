d799ee3efa06cd474461666107294db00f245fec
zjhch123 <416836508@qq.com>
2020-04-14 23:02:25 +0800

    Feat: eslint and upgrade dependencies

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-const param = [1, 2, 3]
+const param = [1, 2, 3];
 
-const delay = () => new window.Promise(res => setTimeout(res, ~~([5, ...param].join(''))))
+const delay = () => new window.Promise(res => setTimeout(res, ~~([5, ...param].join(''))));
 
 export const greeting = () => {
-  console.log('HelloWorld!')
-}
+  console.log('HelloWorld!');
+};
 
 export const testAwait = async () => {
-  await delay()
-  console.log(666)
-}
+  await delay();
+  console.log(666);
+};
 
 export default function () {
-  console.log('export default')
-}
+  console.log('export default');
+};
